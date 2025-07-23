9c265058ab89e43597de3404e51afe32d048bb1b
Bryan Nebres <nebresb@gmail.com>
2025-07-23 20:53:17 +0800

    feat (AuthentiFact Commit V1): Add UI Components, File Structure

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './pages/MainPage/index.js';
 
 test('renders learn react link', () => {
   render(<App />);
