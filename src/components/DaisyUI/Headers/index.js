3c932f6519d32a47ce128831b8ff3755db220d04
Bryan Nebres <nebresb@gmail.com>
2025-07-28 21:04:03 +0800

    feat (Authentifact Commit V1.1): Add Headers, Tabs, Tailwind Config via React (Not Vite), PostCSS, File Structure V1.1

---
+++ src/components/DaisyUI/Headers/index.js
@@ -0,0 +1,25 @@
+function Headers() {
+  return (
+  <div class="navbar bg-base-100 shadow-sm">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="navbar-center">
+    <a class="btn btn-ghost text-xl">FactCheck Pro</a>
+    <a class="text-s">Advanced Al-powered fact-checking and news verification system</a>
+  </div>
+</div>
+  );
+}
+
+export default Headers;
---
+++ src/components/DaisyUI/Tabs/index.js
@@ -0,0 +1,11 @@
+function Tabs() {
+  return (
+     <div role="tablist" class="tabs">
+         <a role="tab" class="tab">Instant Analysis</a>
+         <a role="tab" class="tab tab-active">Multi-Source Verification</a>
+         <a role="tab" class="tab">Expert Grade Accuracy</a>
+     </div>
+  );
+}
+
+export default Tabs;
